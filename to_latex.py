9437653f494d61cd2fa6ed6e42ec574675d4c40c
Maximilian Remming <maxremming@gmail.com>
2017-03-12 20:03:39 +0200

    Revert "support backslash"
    This reverts commit 13cea7cd52446e786233643ed6450933b2f1c505.

--- to_latex.py
+++ to_latex.py
@@ -36,7 +36,6 @@ def line_hack(line, is_line=True):
     line = line.replace("|", "\\|")
     line = line.replace("@", "\\@")
     line = line.replace("$", "\\$")
-    line = line.replace("\\", "\\textbackslash{}")
     line = line.replace("€", "\\euro{}")
     line = line.replace("+", "\\texttt{+}")
     line = line.replace(";,;", ":,:")
